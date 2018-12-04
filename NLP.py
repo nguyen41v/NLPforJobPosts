8fd9e9c0db181a7672410206255be33f9f1a9834
nguyen41v <nguyen41v@gmail.com>
2018-12-04 02:19:59 -0800

    Made a class to keep track of all of the data and a preliminary function to read job descriptions

--- NLP.py
+++ NLP.py
@@ -8,22 +8,53 @@ def preprocess(sent):
     sent = nltk.pos_tag(sent)
     return sent
 
+class heatmapLevelGenerator():
+    def __init__(self):
+        self.languages = []
+        with open('txtFileofLanguages.txt', 'r') as txtfile:
+            reader = txtfile.readlines()
+            for line in reader:
+                self.languages.append(line.strip())
+
+        self.valueOne = []
+        with open('valueOne.txt', 'r') as txtfile:
+            reader = txtfile.readlines()
+            for line in reader:
+                print(line.strip())
+                self.valueOne.append(line.strip())
+
+        self.valueTwo = []
+        with open('valueOne.txt', 'r') as txtfile:
+            reader = txtfile.readlines()
+            for line in reader:
+                self.valueTwo.append(line.strip())
+
+        self.valueNegative = []
+        with open('valueOne.txt', 'r') as txtfile:
+            reader = txtfile.readlines()
+            for line in reader:
+                self.valueNegative.append(line.strip())
+
+
+    def reader(self, filename):
+        dictOfLanguageScores = {}
+        with open(filename, 'r') as txtfile:
+            reader = txtfile.readlines()
+            for line in reader:
+                words = line.split(' ') # not accounting for languages that have spaces in them for now
+                for word in words:
+                    if word in self.languages:
+                        try:
+                            dictOfLanguageScores[word]
+                        except KeyError:
+                            dictOfLanguageScores[word] = 0
+
 
 
 
 
 if __name__ == '__main__':
-    # ex = 'Proficient in at least one (preferably two) of the following languages: Python, Java, C, C++, C#, Ruby, JavaScript, or another object-oriented language '
-    # sent = preprocess(ex)
-    # print(sent)
-    languages = []
-    with open('txtFileofLanguages.txt', 'r') as txtfile:
-        reader = txtfile.readlines()
-        for line in reader:
-            print(line.strip())
-            languages.append(line.strip())
-
-
-
+    hi = heatmapLevelGenerator()
+    hi.reader('test.txt')
 
 
