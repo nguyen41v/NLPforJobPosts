976aece2a8e0438a323c2e0794e7dfea0b04d825
nguyen41v <nguyen41v@gmail.com>
2018-12-04 02:35:29 -0800

    Fixed valueXXX.txt files to contain information in correct format and finished reader function

--- NLP.py
+++ NLP.py
@@ -17,37 +17,57 @@ class heatmapLevelGenerator():
                 self.languages.append(line.strip())
 
         self.valueOne = []
-        with open('valueOne.txt', 'r') as txtfile:
-            reader = txtfile.readlines()
-            for line in reader:
-                print(line.strip())
+        with open('valueOne.txt', 'r') as txtfile1:
+            reader1 = txtfile1.readlines()
+            for line in reader1:
                 self.valueOne.append(line.strip())
 
         self.valueTwo = []
-        with open('valueOne.txt', 'r') as txtfile:
-            reader = txtfile.readlines()
-            for line in reader:
+        with open('valueTwo.txt', 'r') as txtfile2:
+            reader2 = txtfile2.readlines()
+            for line in reader2:
                 self.valueTwo.append(line.strip())
 
         self.valueNegative = []
-        with open('valueOne.txt', 'r') as txtfile:
-            reader = txtfile.readlines()
-            for line in reader:
+        with open('valueNegative.txt', 'r') as txtfile3:
+            reader3 = txtfile3.readlines()
+            for line in reader3:
                 self.valueNegative.append(line.strip())
 
 
     def reader(self, filename):
-        dictOfLanguageScores = {}
+        dict_of_language_scores = {}
         with open(filename, 'r') as txtfile:
             reader = txtfile.readlines()
             for line in reader:
-                words = line.split(' ') # not accounting for languages that have spaces in them for now
+                words = line.strip().split(' ') # not accounting for languages that have spaces in them for now
+                temps = [] # store words found in this line
                 for word in words:
                     if word in self.languages:
+                        if word not in temps:
+                            temps.append(word)
                         try:
-                            dictOfLanguageScores[word]
+                            dict_of_language_scores[word]
                         except KeyError:
-                            dictOfLanguageScores[word] = 0
+                            dict_of_language_scores[word] = 0
+                # assume that all words apply to all languages that appear in that sentence
+                for word in words:
+                    if word in self.valueOne:
+                        for temp in temps:
+                            dict_of_language_scores[temp] += 1
+                    if word in self.valueTwo:
+                        for temp in temps:
+                            dict_of_language_scores[temp] += 2
+                    if word in self.valueNegative:
+                        for temp in temps:
+                            dict_of_language_scores[temp] -= 1
+        for key, value in dict_of_language_scores.items():
+            heatmap_value = 1
+            if value >= 2:
+                heatmap_value = 2
+            if value <= 0:
+                heatmap_value = 0
+            print(key, heatmap_value)
 
 
 
