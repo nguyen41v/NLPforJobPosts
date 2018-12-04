6a36af2e80b972e8cf6dbde0767fd0365d4e2491
nguyen41v <nguyen41v@gmail.com>
2018-12-03 22:50:11 -0800

    Added files of languages separated by newlines

--- NLP.py
+++ NLP.py
@@ -1,2 +1,19 @@
 import nltk
+import re
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
+
+def preprocess(sent):
+    sent = nltk.word_tokenize(sent)
+    sent = nltk.pos_tag(sent)
+    return sent
+
+
+
+
+
+if __name__ == '__main__':
+    ex = 'Proficient in at least one (preferably two) of the following languages: Python, Java, C, C++, C#, Ruby, JavaScript, or another object-oriented language '
+
+
 
