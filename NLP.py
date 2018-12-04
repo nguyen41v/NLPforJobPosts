f26e43e42b52ba62040a11c633e2c4cdf608bf2e
nguyen41v <nguyen41v@gmail.com>
2018-12-04 04:21:15 -0800

    Added while loop to clearTags :))

--- NLP.py
+++ NLP.py
@@ -71,10 +71,43 @@ class heatmapLevelGenerator():
                 heatmap_value = 0
             print(key, heatmap_value)
 
+    def readerWeb(self, text):
+        dict_of_language_scores = {}
+        reader = text.split('.')
+        for line in reader:
+            words = line.strip().split(' ') # not accounting for languages that have spaces in them for now
+            temps = [] # store words found in this line
+            for word in words:
+                if word in self.languages:
+                    if word not in temps:
+                        temps.append(word)
+                    try:
+                        dict_of_language_scores[word]
+                    except KeyError:
+                        dict_of_language_scores[word] = 0
+            # assume that all words apply to all languages that appear in that sentence
+            for word in words:
+                if word in self.valueOne:
+                    for temp in temps:
+                        dict_of_language_scores[temp] += 1
+                if word in self.valueTwo:
+                    for temp in temps:
+                        dict_of_language_scores[temp] += 2
+                if word in self.valueNegative:
+                    for temp in temps:
+                        dict_of_language_scores[temp] -= 1
+        for key, value in dict_of_language_scores.items():
+            heatmap_value = 1
+            if value >= 2:
+                heatmap_value = 2
+            if value <= 0:
+                heatmap_value = 0
+            print(key, heatmap_value)
+
     def printJD(self):
         # the target we want to open
-        # url = input("Copy the LinkedIn Job description page here: ")
-        url = "https://job-openings.monster.com/application-developer-servicenow-irving-tx-us-christus-health/22/3fd9454f-7271-4904-b74e-05f8dd149e9c"
+        url = input("Copy the Monster Job description page here: ")
+        # url = "https://job-openings.monster.com/application-developer-servicenow-irving-tx-us-christus-health/22/3fd9454f-7271-4904-b74e-05f8dd149e9c"
         classname = "jobs-box__html-content jobs-description-content__text t-14 t-black--light t-normal"
 
         # open with GET method
@@ -92,25 +125,21 @@ class heatmapLevelGenerator():
             # jd = soup.find("ul", id="mylist")
             # jd = soup.find("ul",{"class":classname}).text
             # jd = soup.find("span", {"class": classname, "data-value": True})['data-value']
-            jd = soup.find(id='JobDescription')
-            jd.prettify(formatter=None)
-            print(jd)
+            jd = str(soup.find(id='JobDescription'))
+            # print(jd)
+            # print(self.clearTags(jd))
+            return self.clearTags(jd)
+            # print(jd)
         else:
             print("Error")
 
     def clearTags(self, text):
-        counter = 0
-        start = 0
-        finish = 0
-        cleanText = ""
+        while text.rfind('<') != -1:
+            text = text[:text.rfind('<')] + ' ' + text[text.rfind('>') + 1:]
+        return text.strip().replace('     ', ' ')
 
-        for i in text:
-            counter += 1
-            if i == ">":
-                start = counter + 1
 
 if __name__ == '__main__':
     hi = heatmapLevelGenerator()
-    hi.reader('test.txt')
-
+    hi.readerWeb(hi.printJD())
 
