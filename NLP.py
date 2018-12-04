2b7ad674df7c351867d2cc5d648df27dabdf3478
nguyen41v <nguyen41v@gmail.com>
2018-12-04 12:56:15 -0800

    Made a main function so people can keep putting in job descriptions

--- NLP.py
+++ NLP.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import operator
 
 
 class heatmapLevelGenerator():
@@ -34,6 +35,53 @@ class heatmapLevelGenerator():
         with open(filename, 'r') as txtfile:
             reader = txtfile.readlines()
             for line in reader:
+                words = line.strip().split(' ')  # not accounting for languages that have spaces in them for now
+                temps = []  # store words found in this line
+                for word in words:
+                    if word in self.languages:
+                        if word not in temps:
+                            temps.append(word)
+                        try:
+                            dict_of_language_scores[word]
+                        except KeyError:
+                            dict_of_language_scores[word] = 0
+                # assume that all words apply to all languages that appear in that sentence
+                for word in words:
+                    if word in self.valueOne:
+                        for temp in temps:
+                            dict_of_language_scores[temp] += 1
+                    if word in self.valueTwo:
+                        for temp in temps:
+                            dict_of_language_scores[temp] += 2
+                    if word in self.valueNegative:
+                        for temp in temps:
+                            dict_of_language_scores[temp] -= 1
+        print("\nRequired programming languages are as follow:")
+        for key, value in sorted(dict_of_language_scores.items(), key=operator.itemgetter(1), reverse=True):
+            heatmap_value = 1
+            if value >= 2:
+                heatmap_value = 2
+            if value <= 0:
+                heatmap_value = 0
+            print(key, heatmap_value)
+        print("=" * 44)
+
+
+    def readerInteractive(self):
+        dict_of_language_scores = {}
+        filename = input("Which file is the job description in?\n")
+        filename = filename.strip()
+        while True:
+            try:
+                with open(filename, 'r') as txtfile:
+                    break
+            except FileNotFoundError:
+                filename = input("Sorry the file was not found. Please try entering in the filename again.\n"
+                                 "It might be from a typo or a missing extension.\n")
+                filename = filename.strip()
+        with open(filename, 'r') as txtfile:
+            reader = txtfile.readlines()
+            for line in reader:
                 words = line.strip().split(' ') # not accounting for languages that have spaces in them for now
                 temps = [] # store words found in this line
                 for word in words:
@@ -55,20 +103,22 @@ class heatmapLevelGenerator():
                     if word in self.valueNegative:
                         for temp in temps:
                             dict_of_language_scores[temp] -= 1
-        for key, value in dict_of_language_scores.items():
+        print("\nRequired programming languages are as follow:")
+        for key, value in sorted(dict_of_language_scores.items(), key=operator.itemgetter(1), reverse=True):
             heatmap_value = 1
             if value >= 2:
                 heatmap_value = 2
             if value <= 0:
                 heatmap_value = 0
             print(key, heatmap_value)
+        print("=" * 44)
 
     def readerWeb(self, text):
         dict_of_language_scores = {}
         reader = text.split('.')
         preferred = False
         for line in reader:
-            words = line.strip().split(' ') # not accounting for languages that have spaces in them for now
+            words = line.strip().split(' ')  # not accounting for languages that have spaces in them for now
             temps = [] # store words found in this line
             if 'prefer' in line.lower():
                 preferred = True
@@ -92,27 +142,32 @@ class heatmapLevelGenerator():
                 if word in self.valueNegative:
                     for temp in temps:
                         dict_of_language_scores[temp] -= 1
-        for key, value in dict_of_language_scores.items():
+        print("\nRequired programming languages are as follow:")
+        for key, value in sorted(dict_of_language_scores.items(), key=operator.itemgetter(1), reverse=True):
             heatmap_value = 1
             if value >= 2:
                 heatmap_value = 2
             if value <= 0:
                 heatmap_value = 0
             print(key, heatmap_value)
+        print("=" * 44 + '\n')
 
     def printJD(self):
         # the target we want to open
-        url = input("Copy the Monster Job description page here: ")
+        url = input("Paste the Monster Job description page here.\n")
         url = url.strip()
         classname = "jobs-box__html-content jobs-description-content__text t-14 t-black--light t-normal"
-
-        # open with GET method
-        resp = requests.get(url)
+        while True:
+            # open with GET method
+            try:
+                resp = requests.get(url)
+                break
+            except requests.exceptions.MissingSchema:
+                url = input("Sorry, that link was not valid. Perhaps you meant http://" + url +'\nPlease reenter the url.')
 
         # http_respone 200 means OK status
         if resp.status_code == 200:
             print(" - Successfully opened the web page - ")
-            print("Required programming languages are as follow : \n")
 
             # we need a parser,Python built-in HTML parser is enough .
             soup = BeautifulSoup(resp.text, 'html.parser')
@@ -127,7 +182,7 @@ class heatmapLevelGenerator():
             return self.clearTags(jd)
             # print(jd)
         else:
-            print("Error")
+            print("The webpage was not successfully opened, there may be an error in the address.")
 
     def clearTags(self, text):
         # can make it better by chaning the filler, ex '\n', based off of what is in the brackets
@@ -136,8 +191,36 @@ class heatmapLevelGenerator():
             text = text[:text.rfind('<')] + '\n' + text[text.rfind('>') + 1:]
         return text.strip().replace('     ', ' ')
 
+    def main(self):
+        direction = input("Hello. This program can parse out programming languages from a job description in a text file or from a job post on Monster.\n"
+                          "It will generate levels to them, where 2 means you need to know the language very well,\n"
+                          "                                       1 means you should have some knowledge of the language,\n"
+                          "                                       0 means you don't need to know the language, but if you do, it's a bonus for the company.\n\n"
+                          "What would you like to do?\n"
+                          "Enter in \"Text\" to parse from a textfile, \"Monster\" to parse from Monster, or \"Quit\" to quit.\n")
+        direction = direction.lower()
+        while direction != "quit" and direction != 'no' and direction != 'exit':
+            if "text" in direction:
+                self.readerInteractive()
+                direction = input(
+                    "Is there anything else you would like to do?\n"
+                    "For reference, enter in \"Text\" to parse from a textfile, \"Monster\" to parse from Monster, or \"Quit\" to quit.\n")
+                direction = direction.lower()
+            elif "monster" in direction:
+                self.readerWeb(self.printJD())
+                direction = input(
+                    "Is there anything else you would like to do?\n"
+                    "For reference, enter in \"Text\" to parse from a textfile, \"Monster\" to parse from Monster, or \"Quit\" to quit.\n")
+                direction = direction.lower()
+            else:
+                direction = input("Sorry that was not a valid input. Please enter in your choice again.\n"
+                                  "For reference, enter in \"Text\" to parse from a textfile, \"Monster\" to parse from Monster, or \"Quit\" to quit.\n")
+        print("Good luck job hunting.")
+
 
 if __name__ == '__main__':
     hi = heatmapLevelGenerator()
-    hi.readerWeb(hi.printJD())
-
+    # hi.reader('test.txt')
+    hi.main()
+    # hi.readerWeb(hi.printJD())
+    # hi.readerInteractive()
